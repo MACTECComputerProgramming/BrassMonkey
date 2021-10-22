b2d388181b105d45794d4b410d08f5c066509dd2
MessyAttorney16 <70332474+MessyAttorney16@users.noreply.github.com>
2021-10-22 10:55:26 -0500

    quaternion is possibly fix to rotation issues

--- New Unity Project/Assets/Scripts/BikeLean.cs
+++ New Unity Project/Assets/Scripts/BikeLean.cs
@@ -6,20 +6,21 @@ public class BikeLean : MonoBehaviour
     public float smooth;
     public GameObject myObject;
     public float angle;
-
+    //public Quaternion left
    
     private void Update()
     {
-       //Rotation to Left
-        if (Input.GetAxis("Horizontal") <= -0.1 && myObject.transform.rotation.eulerAngles.z <= 20)
+        //float rotation = Quaternion.Angle(transform.rotation, )
+        //Rotation to Left
+        if (Input.GetAxis("Horizontal") <= -0.1)
         {
             targetRotation = Quaternion.AngleAxis(75f, transform.forward) * transform.rotation;
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smooth * Time.deltaTime);
             Debug.Log("Turning Right");
         }
-        
+
         //Rotation to Right
-        if (Input.GetAxis("Horizontal") >= 0.1 && myObject.transform.rotation.eulerAngles.z >= -20)
+        if (Input.GetAxis("Horizontal") >= 0.1)
         {
             targetRotation = Quaternion.AngleAxis(-75f, transform.forward) * transform.rotation;
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smooth * Time.deltaTime);
@@ -33,26 +34,6 @@ public class BikeLean : MonoBehaviour
         }
         
         
-        ////Rotation to Right
-        //if (Input.GetAxis("Horizontal") >= 0.1 && myObject.transform.rotation.eulerAngles.z >= -20)
-        //{
-        //    targetRotation = Quaternion.AngleAxis(-75f, transform.forward) * transform.rotation;
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smooth * Time.deltaTime);
-        //    Debug.Log("Turning Left");
-        //}
-        ////Rotation to Left
-        //if (Input.GetAxis("Horizontal") <= -0.1 && myObject.transform.rotation.eulerAngles.z <= 20)
-        //{
-        //    targetRotation = Quaternion.AngleAxis(75f, transform.forward) * transform.rotation;
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smooth * Time.deltaTime);
-        //    Debug.Log("Turning Right");
-        //}
-        //else
-        //{
-        //    angle = myObject.transform.rotation.eulerAngles.z;
-        //    targetRotation = Quaternion.AngleAxis(-angle, transform.forward) * transform.rotation;
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smooth * Time.deltaTime);
-        //}
-
+       
     }
 }

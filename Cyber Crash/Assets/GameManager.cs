e1db74eff1c2b5bc66ab3381bd3c1dfec5012f33
Drewwheeler2 <70332740+Drewwheeler2@users.noreply.github.com>
2021-10-28 10:58:21 -0500

    doob did work

---
+++ Cyber Crash/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+
+    bool gameHasEnded = false;
+    
+    public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        gameHasEnded = true;
+        Debug.Log("GAME OVER");
+        //Restart game
+    }
+
+
+
+
+
+}

ba5abf47e86be6b06e1f8535092709981f66739a
MessyAttorney16 <ljhalstead16@gmail.com>
2021-11-10 08:47:50 -0600

    WOrk

---
+++ Cyber Crash/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,35 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerController movement;     //A referecne to our PlayerMovement Script
+    
+
+
+    //This function runs when we hit another object
+    //We get information about the collision and call it "collisionInfo".
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //We check if the object we collided with has a tag called "Obstacle".
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;   //Disabled the players movement
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cyber Crash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float speed = 5;
+    public Rigidbody rb;
+    public float sideSpeed = 10;
+
+
+    float horizontalInput;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = new Vector3(0, 0, -horizontalInput);
+        
+        rb.MovePosition(rb.position + forwardMove + horizontalMove * sideSpeed);
+        //horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
